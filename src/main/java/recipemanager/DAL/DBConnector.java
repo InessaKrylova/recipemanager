c203d7a61e9ed0590b466439f5ddf50c00f63c78
Inessa Krylova <Inessa_Krylova@epam.com>
2019-03-22 18:16:17 +0400

    DBConnector methods removed

--- src/main/java/recipemanager/DAL/DBConnector.java
+++ src/main/java/recipemanager/DAL/DBConnector.java
@@ -25,29 +25,5 @@ public class DBConnector {
         Class.forName("org.postgresql.Driver");
         con = DriverManager.getConnection(url, login, password);
         return con;
-    }   
-    
-    public static void connect(){
-        try { 
-            Class.forName("org.postgresql.Driver");
-            con = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/recipeDB","postgres", "123456");
-            System.out.println("Enabled connection with recipeDB.\n");
-        }  catch (Exception e) {
-            System.out.println("Error of connection! "+ e.getMessage());
-        }
-    }
-    
-     
-    public static Connection getConnection() {
-    	return con;
-    }
-    
-    public static void exit() {
-        try { 
-        	con.close(); 
-        	System.out.println("\nDisabled connection with recipeDB.");
-        } catch (Exception e) {
-        	System.out.println("Error of connection! "+e.getMessage());
-        }
-    }
+    }      
 }
